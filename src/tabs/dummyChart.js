e517ef3b239b8964bfb8ded765e6f5700d748fd1
vladikcoder <optimus014@gmail.com>
2019-05-25 00:55:37 +0300

    Tabs component was implemented

---
+++ src/tabs/dummyChart.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+
+const dummyChart = () => {
+  return (
+    <div className="Tabs-container">
+      <b>Lorem</b> ipsum dolor sit amet, consectetur adipisicing elit.
+      Ab aliquid consequuntur laudantium magnam modi sit tempora
+      ullam ut. Debitis dolor doloribus, facilis fugit inventore
+      ipsam ipsum iste maxime qui totam.
+    </div>
+  )
+};
+
+export default dummyChart;
---
+++ src/tabs/dummyTable.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+
+const dummyTable = () => {
+  return (
+    <div className="Tabs-container">
+      <b>Lorem</b> ipsum dolor sit amet, consectetur adipisicing elit.
+      Earum error eum, expedita fugit ipsum officia perspiciatis
+      quaerat repellat voluptatibus? Debitis inventore laboriosam
+      magnam magni pariatur unde voluptates! Aperiam, facilis, velit?
+    </div>
+  )
+};
+
+export default dummyTable;
